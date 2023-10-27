a3c480f7e8b73ad9717a069403dc87ebf3c0b47a
Takasur Azeem <takasurazeem@gmail.com>
2023-10-28 03:22:22 +0500

    Initial Commit

---
+++ BestImageEditingApp/BestImageEditingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BestImageEditingAppApp.swift
+//  BestImageEditingApp
+//
+//  Created by Takasur Azeem on 28/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct BestImageEditingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BestImageEditingApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BestImageEditingApp
+//
+//  Created by Takasur Azeem on 28/10/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}

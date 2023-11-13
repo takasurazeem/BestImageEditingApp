5d930f958bd06e895a8a29449bc1344a79f7c687
Takasur Azeem <takasurazeem@gmail.com>
2023-11-13 11:53:17 +0500

    Move code to ViewModel and inject ViewModel.

--- BestImageEditingApp/BestImageEditingAppApp.swift
+++ BestImageEditingApp/BestImageEditingAppApp.swift
@@ -11,7 +11,9 @@ import SwiftUI
 struct BestImageEditingAppApp: App {
     var body: some Scene {
         WindowGroup {
-            InvertImageView()
+            InvertImageView(
+                viewModel: InvertImageView.ViewModel()
+            )
         }
     }
 }
